ed62f0d75f84a6adb7a2a52a16c02b1b3a4a3d12
Philippe Blanc <p@visualfox.me>
2021-01-09 13:06:46 +0100

    fix DOMMatrix is not defined error for node

--- src/util/DOMMatrix.js
+++ src/util/DOMMatrix.js
@@ -1,5 +1,5 @@
 import CSSMatrix from 'dommatrix/src/index.js'
 
-let CSS3Matrix = typeof DOMMatrix !== undefined ? DOMMatrix : CSSMatrix
+let CSS3Matrix = typeof DOMMatrix !== 'undefined' ? DOMMatrix : CSSMatrix
 
 export default CSS3Matrix
