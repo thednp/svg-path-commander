0e2e0c709f5d250d50ca5afe3477eb618bfbb7dc
thednp <dnp.digital@gmail.com>
2022-05-13 18:27:40 +0300

    Update esbuild-istanbul.js

--- cypress/plugins/esbuild-istanbul.js
+++ cypress/plugins/esbuild-istanbul.js
@@ -11,7 +11,7 @@ sourceFolder = sourceFolder || 'src';
 const [name] = process.cwd().split(/[\\|\/]/).slice(-1);
 const sourcePath = sourceFolder.replace(/\\/g,'\/');
 
-const sourceFilter = `${name}\\${sourcePath}`;
+const sourceFilter = `${name}/${sourcePath}`;
 const instrumenter = createInstrumenter({
   compact: false,
   esModules: true,
@@ -23,8 +23,9 @@ const esbuildPluginIstanbul = () => ({
     build.onLoad({filter: /\.(js|jsx|ts|tsx)$/ },
       async ({ path }) => {
         const contents = String(readFileSync(path, 'utf8'));
+        const samePath = path.replace(/\\/g, '/')
 
-        if (!path.includes(sourceFilter)) {
+        if (!samePath.includes(sourceFilter)) {
           return { contents };
         }
         debug('instrumenting %s for output coverage', path);
